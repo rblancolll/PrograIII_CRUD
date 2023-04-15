14d7c119232ddae69e905bf72e4d218fbf99769c
rblancolll <rblancol@miumg.edu.gt>
2023-04-14 21:16:24 -0600

    se finalizo el crud

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\PersonasController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PersonasController::class, 'index'])->name('personas.index');
+Route::get('/create', [PersonasController::class, 'create'])->name('personas.create');
+Route::post('/store', [PersonasController::class, 'store'])->name('personas.store');
+Route::get('/edit/{id}', [PersonasController::class, 'edit'])->name('personas.edit');
+Route::put('/update/{id}', [PersonasController::class, 'update'])->name('personas.update');
+Route::get('/show/{id}', [PersonasController::class, 'show'])->name('personas.show');
+Route::delete('/destroy/{id}', [PersonasController::class, 'destroy'])->name('personas.destroy');
